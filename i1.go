d15b69620792f118576eb2318e0b63f7b16437a6
Sagar Puneria <spuneria@rythmos.com>
2019-02-19 16:29:22 +0530

    root commit

---
+++ i1.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Mover interface {
+	Move()
+}
+
+type Car struct {
+	price int
+	Mover
+	speed int
+}
+
+type Wheel struct {
+}
+
+func (w Wheel) Move() {
+	fmt.Println("Inside wheel")
+}
+
+// promoted method
+func (c Car) Move() {
+	fmt.Println("Inside Move")
+	c.Mover.Move() // Crashes, nil pointer dereference. If Field 'Mover' is nil
+}
+
+func main() {
+	//var c Car
+	var m Mover = Car{Mover: Wheel{}}
+	//var m Mover = Car{} // Field 'Mover' is nil.
+	m.Move()
+}
