388326ac2216e1876b3928f04212c1c6de3017d5
Sagar Puneria <spuneria@rythmos.com>
2019-02-19 16:25:25 +0530

    code refactor

--- objectStruct.go
+++ objectStruct.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+type interfaceB interface {
+	object()
+}
+
 type A struct {
 	name string
 }
@@ -16,11 +20,18 @@ func (d *A) object() { // Correct way
 }
 
 func main() {
-	//a := A{} // (OR) var a A
-	a := &A{}
+	a := A{} // var a A
+	// (OR)
+	//a := &A{}
 	fmt.Println("a:", a)
 	a.name = "sagar"
 	fmt.Println("a.name:", a.name)
 	a.object()
 	fmt.Println("a.name:", a.name)
+
+	var b interfaceB
+	b = &a // In case a := A{} // var a A
+	// b = a // In case a := &A{}
+	// In interface type, If the method is receiver pointer, then object must also be pointer.
+	b.object()
 }
