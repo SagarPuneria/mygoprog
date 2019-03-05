6d51bd712dbbe9676a679a5cc044b54192aa6b1d
Sagar Puneria <spuneria@rythmos.com>
2019-03-05 23:30:45 +0530

    commmit

--- mergeSorting.go
+++ mergeSorting.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+)
+
+func Merge(l, r []int) []int {
+	ret := make([]int, 0, len(l)+len(r))
+	for len(l) > 0 || len(r) > 0 {
+		if len(l) == 0 {
+			return append(ret, r...)
+		}
+		if len(r) == 0 {
+			return append(ret, l...)
+		}
+		if l[0] <= r[0] {
+			ret = append(ret, l[0])
+			l = l[1:]
+		} else {
+			ret = append(ret, r[0])
+			r = r[1:]
+		}
+	}
+	return ret
+}
+
+func MergeSort(s []int) []int {
+	if len(s) <= 1 {
+		return s
+	}
+	n := len(s) / 2
+	l := MergeSort(s[:n])
+	r := MergeSort(s[n:])
+	return Merge(l, r)
+}
+
+func main() {
+	s := []int{9, 4, 3, 6, 1, 2, 10, 5, 7, 8}
+	fmt.Printf("%v\n%v\n", s, MergeSort(s))
+}
+
+/*package main
+
 import "fmt"
 
 func main() {
@@ -49,4 +90,4 @@ func merge(a []int, l, m, h int) {
 	for p = 0; p <= h; p++ {
 		a[p] = b[p]
 	}
-}
+}*/
