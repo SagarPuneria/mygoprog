7212f1529b0f09274700ae98896bae6c2d0d1143
Sagar Puneria <spuneria@rythmos.com>
2019-03-12 22:30:15 +0530

    ncoisnvcs

--- channel1.go
+++ channel1.go
@@ -6,18 +6,23 @@ import (
 )
 
 func main() {
-	messages := make(chan string)
+	message := make(chan string)
 	go func() {
-		messages <- "ping"         // Here control holds execution until variable messages send any string. If won't send any string it will be deadlock.
-		fmt.Println(len(messages)) // 0
+		message <- "ping"                          // Here control holds execution until variable message send any string. If won't send any string it will be deadlock.
+		fmt.Println("len(message):", len(message)) // len(message): 0
 	}()
 	time.Sleep(2 * time.Second)
-	msg := <-messages // Here control holds execution until variable messages received any string. If won't received any string it will be deadlock.
-	fmt.Println(msg)  // ping
+	msg := <-message // Here control holds execution until variable message received any string. If won't received any string it will be deadlock.
+	fmt.Println(msg) // ping
+	time.Sleep(1 * time.Second)
 }
 
 /*
 e:\workspace\TDD\gorilla_mux\src\golang-network-programming>go run channel1.go
-0
+len(message):0
 ping
+[OR]
+e:\workspace\TDD\gorilla_mux\src\golang-network-programming>go run channel1.go
+ping
+len(message): 0
 */
