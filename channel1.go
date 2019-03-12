575e6624fcfef57aca21d37bef9d4f52f4997e8e
Sagar Puneria <spuneria@rythmos.com>
2019-03-12 22:16:51 +0530

    ncoisnvcs

--- channel1.go
+++ channel1.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	messages := make(chan string)
-	go func() { messages <- "ping" }()
+	go func() {
+		messages <- "ping"         // Here control holds execution until variable messages send any string. If won't send any string it will be deadlock.
+		fmt.Println(len(messages)) // 0
+	}()
+	time.Sleep(2 * time.Second)
 	msg := <-messages // Here control holds execution until variable messages received any string. If won't received any string it will be deadlock.
 	fmt.Println(msg)  // ping
 }
+
+/*
+e:\workspace\TDD\gorilla_mux\src\golang-network-programming>go run channel1.go
+0
+ping
+*/
