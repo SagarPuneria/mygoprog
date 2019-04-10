7b89966a6dd26ed909e67a22e1744e6fa026ebb1
Sagar Puneria <sagar.puneria@gmail.com>
2019-04-10 21:42:33 +0530

    code refactor

---
+++ restAPI/b4.go
@@ -0,0 +1,105 @@
+/*
+Processing HTTP requests with Go is primarily about two things:
+(1) ServeMux aka Request Router aka MultiPlexor
+(2) Handlers
+
+SERVEMUX
+ServeMux = HTTP request router = multiplexor = mux
+compares incoming requests against a list of predefined URL paths,
+and calls the associated handler for the path whenever a match is found.
+
+HANDLERS
+responsible for writing response headers and bodies.
+Almost any type ("object") can be a handler, so long as it satisfies the http.Handler interface.
+In lay terms, that simply means it must have a ServeHTTP method with the following signature:
+ServeHTTP(http.ResponseWriter, *http.Request)
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type person struct {
+	fName string
+}
+
+func (p *person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("First Name: " + p.fName)
+	w.Write([]byte("First Name: " + p.fName))
+}
+
+func main() {
+
+	//http.ListenAndServe(":8080", &person{fName: "Jim"})
+
+	/*personOne := &person{fName: "Jim"}
+	http.ListenAndServe(":8080", personOne)*/
+
+	/*http.Handle("/", &person{fName: "Jim"})
+	//http.Handle("/first", &person{fName: "first"})
+	http.Handle("/first", http.RedirectHandler("https://www.gorillatoolkit.org/pkg/mux", 307))
+	http.ListenAndServe(":8080", nil)*/
+
+	/*mux := http.NewServeMux()
+	mux.Handle("/", &person{fName: "Jim"})
+	//mux.Handle("/first", &person{fName: "first"})
+	mux.Handle("/first", http.RedirectHandler("https://www.gorillatoolkit.org/pkg/mux", 307))
+	mux.HandleFunc("/all", someFunc)
+	http.ListenAndServe(":8080", mux)*/
+
+	//-------------------------------------
+	/*http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("Hello universe 2")
+		w.Write([]byte("Hello universe 2"))
+		//req.Body.Close() // NOT NEEDED
+		//req.Close = true // NOT NEEDED FOR SERVER SIDE
+	})*/
+	http.HandleFunc("/all", someFunc)
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
+
+func someFunc(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("%+v\n", req)
+	fmt.Println("==============================================================================")
+	fmt.Println("req:", req) // OR fmt.Printf("%v\n", req)
+	fmt.Println("==============================================================================")
+	fmt.Println("req.Method:", req.Method)
+	fmt.Println("req.URL:", req.URL)
+	fmt.Println("req.Proto:", req.Proto, ",req.ProtoProtoMajor:", req.ProtoMajor, ",req.ProtoMinor:", req.ProtoMinor)
+	fmt.Println("req.Header:", req.Header)
+	fmt.Println("req.Body:", req.Body)
+	fmt.Println("req.GetBody:", req.GetBody)
+	fmt.Println("req.ContentLength:", req.ContentLength)
+	fmt.Println("req.TransferEncoding:", req.TransferEncoding)
+	fmt.Println("req.Host:", req.Host)
+	fmt.Println("req.Form:", req.Form)
+	fmt.Println("req.PostForm:", req.PostForm)
+	fmt.Println("req.MultipartForm:", req.MultipartForm)
+	fmt.Println("req.Trailer:", req.Trailer)
+	fmt.Println("req.RemoteAddr:", req.RemoteAddr)
+	fmt.Println("req.RequestURI:", req.RequestURI)
+	fmt.Println("req.TLS:", req.TLS)
+	fmt.Println("req.Cancel:", req.Cancel)
+	fmt.Println("req.Response:", req.Response)
+
+	fmt.Println("Hello universe")
+	w.Write([]byte("Hello universe"))
+}
+
+/*
+We were able to do this because ServeMux also has a ServeHTTP method,
+meaning that it too satisfies the Handler interface.
+
+For me it simplifies things to think of a ServeMux as just being a special kind of handler,
+which instead of providing a response itself passes the request on to a second handler.
+
+This isn't as much of a leap as it first sounds –
+chaining handlers together is fairly commonplace in Go.
+
+from:
+http://www.alexedwards.net/blog/a-recap-of-request-handling
+*/
---
+++ restAPI/b5.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type MyHandler struct {
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+	log.Println("path:", path)
+	//    path := "templates" + r.URL.Path
+
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 My Friend - " + http.StatusText(404)))
+	}
+}
+
+func main() {
+	//http.Handle("/", new(MyHandler))
+	http.Handle("/", &MyHandler{})
+	http.ListenAndServe(":8080", nil)
+}
+
+/*
+structuring go apps
+
+*****************
+SUPER IMPORTANT
+*****************
+
+ROOT is determined by ...
+WHERE YOU ARE IN YOUR DIRECTORY WHEN YOU RUN "GO RUN"
+wherever you are in your dir when you run "go run" - that's your root
+
+for example
+if this is your file structure
+go_web_app
+-- bin
+-- pkg
+-- src
+---- main
+------ main.go
+-- templates
+---- home.html
+
+if you were in "go_web_app / src / main"
+and you ran "go run main.go"
+then the root of your files would be "go_web_app / src / main"
+and you would not be able to access "templates" nor "templates/home.html"
+
+if you were in "go_web_app"
+and you ran "go run main.go"
+then the root of your files would be "go_web_app"
+and you would be able to access "templates" and "templates/home.html"
+
+this is a good article - though advanced (so don't freak out if you're just getting started)
+https://medium.com/@benbjohnson/structuring-applications-in-go-3b04be4ff091
+
+this can also be useful in debugging:
+-- import "os"
+---- log.Println("ENV: ", os.Environ())
+*/
