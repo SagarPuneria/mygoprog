fb36640ce0ef0044cc130e6b08a79c15b1ac7eaf
Sagar Puneria <sagar.puneria@gmail.com>
2020-01-14 15:48:52 +0530

    voends

---
+++ json/j8.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Dummy struct {
+	Name      string `json:"name"`
+	Number    int64
+	Pointer   *string `json:"-"`  // we want to ignore JSON for this one
+	Nopointer string  `json:"-,"` // NoPointer appears in JSON as key "-".
+}
+
+func main() {
+	data := []byte(`
+            {
+                "name": "Mr Dummy",
+                "NuMbeR": 4,
+				"pointer": "yes",
+				"Nopointer": "no"
+            }
+        `)
+
+	var dummy Dummy
+	err := json.Unmarshal(data, &dummy)
+	if err != nil {
+		fmt.Println("An error occured: %v", err)
+		os.Exit(1)
+	}
+
+	// we want to print the field names as well
+	fmt.Printf("struct %+v\n---\n", dummy)
+
+	b, err := json.Marshal(dummy)
+	if err != nil {
+		fmt.Println("An error occured: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("json:", string(b))
+}
